c68a5ba758a389c69f1620440b21f90864411baa
steam88ys <yssteam88@gmail.com>
2022-07-26 15:46:51 +0900

    라디오버튼 목록대화상자 완료

--- app/src/main/java/kr/hs/emirim/s2102/mirim_project_0726_02/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2102/mirim_project_0726_02/MainActivity.java
@@ -1,8 +1,10 @@
 package kr.hs.emirim.s2102.mirim_project_0726_02;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -17,7 +19,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout linear;
-    Button btn2;
+    Button btn2, btn5, btn6;
+    String[] listArr = {"순돌이1", "순돌이2", "순돌이3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,47 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(btn2);
         Button btn3 = findViewById(R.id.btn_toast);
         btn3.setOnClickListener(toastListener);
+        Button btn4 = findViewById(R.id.btn_dialog);
+        btn4.setOnClickListener(dialogListener);
+        btn5 = findViewById(R.id.btn_dialog_list);
+        btn5.setOnClickListener(listDialogListener);
+        btn6 = findViewById(R.id.btn_dialog_radio);
+        btn6.setOnClickListener(radioDialogListener);
     }
+
+    View.OnClickListener radioDialogListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setIcon(R.drawable.icon);
+            dlg.setTitle("제일 귀여운 순돌이 사진은?");
+            dlg.setSingleChoiceItems(listArr,0, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    btn6.setText(listArr[i]);
+                }
+            });
+            dlg.setNegativeButton("닫기", null);
+            dlg.show();
+        }
+    };
+
+    View.OnClickListener listDialogListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+            dlg.setIcon(R.drawable.icon);
+            dlg.setTitle("제일 귀여운 순돌이 사진은?");
+            dlg.setItems(listArr, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    btn5.setText(listArr[i]);
+                }
+            });
+            dlg.setNegativeButton("닫기", null);
+            dlg.show();
+        }
+    };
 
     View.OnClickListener toastListener = new View.OnClickListener() {
         @Override
@@ -40,6 +83,26 @@ public class MainActivity extends AppCompatActivity {
             t.show();
         }
     };
+
+    View.OnClickListener dialogListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setIcon(R.drawable.icon);
+            dialog.setTitle("대화상자연습");
+            dialog.setMessage("여기는 대화상자 내용이 들어갑니다.");
+            dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    linear.setBackgroundColor(Color.MAGENTA);
+                }
+            });
+            dialog.setNegativeButton("취소", null);
+            dialog.show();
+        }
+    };
+
+
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
